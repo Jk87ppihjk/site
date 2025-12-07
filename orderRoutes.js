cac018861f674d6daafa0340628003f847d6aabc
Jk87ppihjk <tutano172@gmail.com>
2025-12-07 13:37:09 -0300

    Add files via upload

--- orderRoutes.js
+++ orderRoutes.js
@@ -6,6 +6,9 @@ const { verifyToken, isAdmin } = require('./authMiddleware');
 router.post('/buy', verifyToken, orderController.buyProduct);
 router.post('/rent', verifyToken, orderController.rentProduct);
 
+router.get('/my-orders', verifyToken, orderController.getUserOrders);
+router.get('/my-rentals', verifyToken, orderController.getUserRentals);
+
 // Admin view rentals
 router.get('/rentals', verifyToken, isAdmin, orderController.getAllRentals);
 
