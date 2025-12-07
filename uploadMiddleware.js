bfaedb513f23ba584123fb2f3f9ced59c393408f
Jk87ppihjk <tutano172@gmail.com>
2025-12-07 13:15:26 -0300

    Update uploadMiddleware.js

--- uploadMiddleware.js
+++ uploadMiddleware.js
@@ -1,3 +1,7 @@
+const multer = require('multer');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinaryConfig');
+
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
